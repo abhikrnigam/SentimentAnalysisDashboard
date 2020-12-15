926179c00897c02cddf63f1c5260585e41e1a871
Abhishek Kumar <abhikrnigam@gmail.com>
2020-12-15 19:40:24 +0530

    Added static graphs. Database implementation done.

---
+++ lib/ChartPlot.dart
@@ -0,0 +1,328 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ChartPlot extends StatefulWidget {
+  @override
+  _ChartPlotState createState() => _ChartPlotState();
+}
+
+class _ChartPlotState extends State<ChartPlot> {
+  BarChartRodData rodData = new BarChartRodData().copyWith(width: 50);
+  List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text(
+          "The Blues",
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10.0, right: 15.0, left: 15.0, bottom: 8),
+                  child: Center(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 35.0, right: 15.0, left: 15.0),
+                    child: Container(
+                      child: BarChart(
+                        BarChartData(
+                          alignment: BarChartAlignment.spaceAround,
+                          maxY: 20,
+                          barTouchData: BarTouchData(
+                            enabled: false,
+                            touchTooltipData: BarTouchTooltipData(
+                              tooltipBgColor: Colors.transparent,
+                              tooltipPadding: const EdgeInsets.all(0),
+                              tooltipBottomMargin: 0,
+                              getTooltipItem: (
+                                BarChartGroupData group,
+                                int groupIndex,
+                                BarChartRodData rodData,
+                                int rodIndex,
+                              ) {
+                                return BarTooltipItem(
+                                  "",
+                                  //rodData.y.round().toString(),
+                                  TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          titlesData: FlTitlesData(
+                            show: true,
+                            bottomTitles: SideTitles(
+                              showTitles: true,
+                              textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12),
+                              margin: 10,
+                              getTitles: (double value) {
+                                switch (value.toInt()) {
+                                  case 0:
+                                    return 'Abhishek';
+                                  case 1:
+                                    return 'Nikhil';
+                                  case 2:
+                                    return 'Tabassum';
+                                  default:
+                                    return '';
+                                }
+                              },
+                            ),
+                            leftTitles: SideTitles(
+                              showTitles: true,
+                              textStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                              margin: 32,
+                              reservedSize: 14,
+                              getTitles: (value) {
+                                if (value == 0) {
+                                  return '1';
+                                } else if (value == 5) {
+                                  return '2';
+                                } else if (value == 10) {
+                                  return '3';
+                                } else if (value == 15)
+                                  return '4';
+                                else if (value == 20)
+                                  return '5';
+                                else {
+                                  return '';
+                                }
+                              },
+                            ),
+                          ),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          barGroups: [
+                            BarChartGroupData(
+                              x: 0,
+                              barRods: [
+                                BarChartRodData(y: 8, color: Colors.blue),
+                              ],
+                              showingTooltipIndicators: [0],
+                            ),
+                            BarChartGroupData(
+                              x: 1,
+                              barRods: [
+                                BarChartRodData(y: 10, color: Colors.blue)
+                              ],
+                              showingTooltipIndicators: [0],
+                            ),
+                            BarChartGroupData(
+                              x: 2,
+                              barRods: [
+                                BarChartRodData(
+                                  y: 14,
+                                  color: Colors.blue,
+                                )
+                              ],
+                              showingTooltipIndicators: [0],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Material(
+                color: Colors.blue,
+                child: Text(
+                  "Figure : Graph showing depression factor \n between the users.",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+            Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10.0, left: 15.0, right: 15.0, bottom: 10.0),
+                  child: Center(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blue[900],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 25.0, left: 50.0, right: 55.0),
+                  child: LineChart(
+                    LineChartData(
+                      gridData: FlGridData(
+                        show: true,
+                        drawVerticalLine: true,
+                        getDrawingHorizontalLine: (value) {
+                          return FlLine(
+                            color: const Color(0xff37434d),
+                            strokeWidth: 1,
+                          );
+                        },
+                        getDrawingVerticalLine: (value) {
+                          return FlLine(
+                            color: const Color(0xff37434d),
+                            strokeWidth: 1,
+                          );
+                        },
+                      ),
+                      titlesData: FlTitlesData(
+                        show: true,
+                        bottomTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 22,
+                          textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                          getTitles: (value) {
+                            switch (value.toInt()) {
+                              case 2:
+                                return '                                     Total number of users -->';
+                              case 5:
+                                return '';
+                              case 8:
+                                return '';
+                            }
+                            return '';
+                          },
+                          margin: 8,
+                        ),
+                        leftTitles: SideTitles(
+                          showTitles: true,
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                          getTitles: (value) {
+                            switch (value.toInt()) {
+                              case 1:
+                                return '1';
+                              case 2:
+                                return '2';
+                                case 3:
+                                return '3';
+                                case 4:
+                                return '4';
+                                case 5:
+                                return '5';
+                                case 6:
+                                return '6';
+                            }
+                            return '';
+                          },
+                          reservedSize: 28,
+                          margin: 12,
+                        ),
+                      ),
+                      axisTitleData: FlAxisTitleData(
+                        leftTitle:
+                            AxisTitle(showTitle: true, titleText: 'Depressed Users -->',textStyle: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.bold)),
+                      ),
+                      borderData: FlBorderData(
+                          show: true,
+                          border: Border.all(
+                              color: const Color(0xff37434d), width: 1)),
+                      minX: 0,
+                      maxX: 11,
+                      minY: 0,
+                      maxY: 6,
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots: [
+                            FlSpot(0, 3),
+                            FlSpot(2.6, 2),
+                            FlSpot(4.9, 5),
+                            FlSpot(6.8, 3.1),
+                            FlSpot(8, 4),
+                            FlSpot(9.5, 3),
+                            FlSpot(11, 4),
+                          ],
+                          isCurved: true,
+                          colors: gradientColors,
+                          barWidth: 5,
+                          isStrokeCapRound: true,
+                          dotData: FlDotData(
+                            show: false,
+                          ),
+                          belowBarData: BarAreaData(
+                            show: true,
+                            colors: gradientColors
+                                .map((color) => color.withOpacity(0.3))
+                                .toList(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Material(
+                color: Colors.blue,
+                child: Text(
+                  "Figure : Graph showing \nDepressed Users vs Number of Users",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:lottie/lottie.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'HomeScreen.dart';
-import 'WalkthorughScreen.dart';
+import 'ChartPlot.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.light(),
       home: MyHomePage(),
@@ -45,50 +46,108 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Lottie.asset('assets/welcome.json',
-                repeat: true,
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .height * 0.5,
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width * 0.7),
-            Text(
-              "Welcome to The Blues \n Social Media Depression Analysis",
-              style: GoogleFonts.poppins(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            //Spacer(),
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height*0.5,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height*0.48,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height*0.46,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                Center(
+                  child: Lottie.asset('assets/welcome.json',
+                      repeat: true,
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .height * 0.5,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width * 0.7),
+                ),
+              ],
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top:50.0),
+                child: Text(
+                  "The Blues ",
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
               ),
             ),
-            Text(
-              "Click on Next to Continue",
-              style: GoogleFonts.poppins(
-                color: Colors.black,
-                fontSize: 15,
+            Center(
+              child: Text(
+                "Social Media Depression Analysis ",
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top:15.0),
+                child: Text(
+                  "The Application helps in visualisation \nof the data which is fed to it through \nFirebase.\nThis application, with the help of \ncharts is able to do so.",
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ),
             ),
+            Spacer(flex: 9,),
             Align(
               alignment: Alignment.bottomRight,
-              child: FlatButton(
-                color: Colors.blue,
-                child: Text("Next",style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                ),),
-                onPressed: () {
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: FloatingActionButton(
+                  elevation: 5,
+                  onPressed: (){
+                    {
 //                  initialRoute == 'true' ?
-                  Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => WalkthroughScreen()));
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => ChartPlot()));
 //                      :
 //                  Navigator.push(context,
 //                      MaterialPageRoute(builder: (context) => HomeScreen()));
-                },
+                    }
+                  },
+                  child: Text(">",style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ),
               ),
             ),
+            Spacer(flex: 2,),
           ],
         ),
       ),
